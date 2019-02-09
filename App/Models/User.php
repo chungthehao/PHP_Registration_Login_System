8b89650f74f5f544ddae2967247a532faa813ae3
Chung Thế Hào <chungthehao@gmail.com>
2019-02-09 20:22:29 +0700

    Registration form, save user record

--- App/Models/User.php
+++ App/Models/User.php
@@ -12,6 +12,13 @@ use PDO;
 class User extends \Core\Model
 {
 
+    public function __construct($data)
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
     /**
      * Get all the users as an associative array
      *
@@ -22,5 +29,22 @@ class User extends \Core\Model
         $db = static::getDB();
         $stmt = $db->query('SELECT id, name FROM users');
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function save()
+    {
+        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+        $sql = 'INSERT INTO users (name, email, password_hash)
+                VALUES (:name, :email, :password_hash)';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+        $stmt->execute();
     }
 }
