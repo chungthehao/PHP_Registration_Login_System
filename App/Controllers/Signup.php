20d7473f3b173b4b756c795da1996a9b21b2a517
Chung Thế Hào <chungthehao@gmail.com>
2019-02-09 21:33:53 +0700

    Validate data on server, http-header post-redirect-get pattern

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -16,8 +16,25 @@ class Signup extends \Core\Controller {
     {
         $user = new User($_POST);
 
-        $user->save();
+        if ($user->save()) {
+//            View::renderTemplate('Signup/success.html');
 
+            # POST - Redirect - GET pattern
+            header('Location: http://' . $_SERVER['HTTP_HOST'] . '/signup/success', true, 303);
+            exit; // Best practice, browser ko can thong tin gi nua, no chi can chuyen trang (thuc hien GET request theo yeu cau)
+        } else {
+//            echo '<pre>';
+//            var_dump($user->errors);
+
+            View::renderTemplate('Signup/new.html', [
+                'user'  => $user // pass User model instance (old values, validation errors)
+            ]);
+        }
+
+    }
+
+    public function successAction()
+    {
         View::renderTemplate('Signup/success.html');
     }
 
--- App/Models/User.php
+++ App/Models/User.php
@@ -11,6 +11,7 @@ use PDO;
  */
 class User extends \Core\Model
 {
+    public $errors = [];
 
     public function __construct($data)
     {
@@ -33,18 +34,76 @@ class User extends \Core\Model
 
     public function save()
     {
-        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+        $this->validate();
 
-        $sql = 'INSERT INTO users (name, email, password_hash)
+        if (count($this->errors) === 0) {
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (name, email, password_hash)
                 VALUES (:name, :email, :password_hash)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute(); // Returns TRUE on success or FALSE on failure.
+        }
+
+        return false;
+    }
+
+    public function validate()
+    {
+        // Name
+        if ($this->name == '') {
+            $this->errors[] = 'Name is required';
+        }
+
+        // Email address
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+            $this->errors[] = 'Invalid email';
+        }
+
+        if ($this->emailExists($this->email)) {
+            $this->errors[] = 'Email already taken';
+        }
+
+        // Password
+        if ($this->password != $this->password_confirmation) {
+            $this->errors[] = 'Password must match confirmation';
+        }
+
+        if (strlen($this->password) < 6) {
+            $this->errors[] = 'Please enter at least 6 characters for the password';
+        }
+
+        if (preg_match('/.*[a-z]+.*/i', $this->password) ==  0) {
+            $this->errors[] = 'Password needs at least one letter';
+        }
+
+        if (preg_match('/.*\d+.*/i', $this->password) ==  0) {
+            $this->errors[] = 'Password needs at least one number';
+        }
+    }
+
+    protected function emailExists($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
-
-        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 
         $stmt->execute();
+
+        // $stmt->fetch() returns false if no records
+        // Summary:
+        // false        --> false,
+        // (records)    --> true
+        return $stmt->fetch() !== false;
     }
+
 }
